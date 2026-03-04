55f2731913f29babf6c387431c8c945ac2eab406
nguyenduyan4107 <nguyenduyan412007@gmail.com>
2026-03-04 17:22:15 +0700

    extwelve.py

---
+++ chapter03/extwelve.py
@@ -0,0 +1,28 @@
+# Bài 12
+m = int(input("Nhập số hàng: "))
+n = int(input("Nhập số cột: "))
+
+A = []
+B = []
+
+print("Nhập ma trận A:")
+for i in range(m):
+    row = list(map(int, input().split()))
+    A.append(row)
+
+print("Nhập ma trận B:")
+for i in range(m):
+    row = list(map(int, input().split()))
+    B.append(row)
+
+C = []
+
+for i in range(m):
+    row = []
+    for j in range(n):
+        row.append(A[i][j] + B[i][j])
+    C.append(row)
+
+print("Ma trận tổng:")
+for row in C:
+    print(row)
