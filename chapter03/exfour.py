16b4519faf72e5b39198b2d0eba95c74d5528f59
nguyenduyan4107 <nguyenduyan412007@gmail.com>
2026-03-04 17:15:46 +0700

    exfour.py

---
+++ chapter03/exfour.py
@@ -0,0 +1,10 @@
+# Bài 4
+numbers = [5, 2, 8, 5, 1, 5]
+
+numbers.sort()
+print("Tăng dần:", numbers)
+
+numbers.reverse()
+print("Đảo ngược:", numbers)
+
+print("Số lần xuất hiện của 5:", numbers.count(5))
