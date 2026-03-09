28e784834984d2bb0c66fdfbf0da99f36722e16c
nguyenduyan4107 <nguyenduyan412007@gmail.com>
2026-03-09 17:29:28 +0700

    exetwo.py

---
+++ exam/exetwo.py
@@ -0,0 +1,12 @@
+print("Các số lẻ từ 17 đến 111 theo thứ tự giảm dần:")
+
+for i in range(111, 16, -1):
+    if i % 2 != 0:
+        print(i, end=" ")
+
+print("\n")
+
+print("Các số nguyên từ 17 đến 111:")
+
+for i in range(17, 112):
+    print(i, end=" ")
