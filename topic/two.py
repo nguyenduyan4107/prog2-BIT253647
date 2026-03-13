d5344a5efc92533a8b5c9e70574936ee23382b92
nguyenduyan4107 <nguyenduyan412007@gmail.com>
2026-03-13 17:33:48 +0700

    two.py

---
+++ topic/two.py
@@ -0,0 +1,10 @@
+import math
+
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+
+print("Lũy thừa a^b =", a**b)
+print("Căn bậc 2 của a =", math.sqrt(a))
+print("Chia lấy phần nguyên:", int(a//b))
+print("Chia lấy phần dư:", a % b)
+print("Làm tròn a:", round(a))
