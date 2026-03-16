04b199d2a7683eeb4af6c32d55f82dee3ec6468d
nguyenduyan4107 <nguyenduyan412007@gmail.com>
2026-03-16 18:11:45 +0700

    Create bai10.py

---
+++ luyentap9/bai10.py
@@ -0,0 +1,13 @@
+class SinhVien:
+
+    def __init__(self, diem):
+        self.diem = diem
+
+    def __eq__(self, other):
+        return self.diem == other.diem
+
+
+sv1 = SinhVien(8)
+sv2 = SinhVien(9)
+
+print(sv1 == sv2)
