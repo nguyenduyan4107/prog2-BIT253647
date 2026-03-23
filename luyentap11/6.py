ad97145f566a936a2538eca74f419678a1e6f998
nguyenduyan4107 <nguyenduyan412007@gmail.com>
2026-03-23 17:23:37 +0700

    Create 6.py

---
+++ luyentap11/6.py
@@ -0,0 +1,13 @@
+d = {}
+
+n = int(input("Nhập số người: "))
+
+for i in range(n):
+    name = input("Tên: ")
+    age = int(input("Tuổi: "))
+    d[name] = age
+
+tong = sum(d.values())
+avg = tong / len(d)
+
+print("Tuổi trung bình:", avg)
